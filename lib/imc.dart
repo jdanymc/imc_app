94af5ba5985905013d96bb1175e5082768626c8b
Jhon Dany Mamani <jdany.mc@gmail.com>
2023-11-03 23:23:04 -0500

    Culminación de aplicación

---
+++ lib/imc.dart
@@ -0,0 +1,14 @@
+class IMC {
+  double min = 0;
+  double max = 0;
+  String nombre = '';
+  String descripcion = '';
+  String url = '';
+
+  IMC(
+      {required this.min,
+      required this.max,
+      required this.nombre,
+      required this.descripcion,
+      required this.url});
+}
